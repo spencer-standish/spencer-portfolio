fe2c7e7f09e34d448db794eca3bf57373fdad0aa
standalonious <97482188+standalonious@users.noreply.github.com>
2023-12-21 16:22:22 -0500

    added avatar img

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img className="object-cover object-center rounded" alt="hero" src="./logo192.png" />
+          <img className="rounded-circle" alt="profile" src="./images/avatar.jpeg" />
         </div>
       </div>
     </section>
